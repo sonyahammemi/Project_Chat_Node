40ab6efecbebf44712de66c6f9d76fddecd79dc4
sonyahammemi <sonya.hammemii@gmail.com>
2022-02-25 12:31:30 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require ('express');
+const cors = require ('cors')
+const Pusher = require("pusher");
+
+const pusher = new Pusher({
+  appId: "1353078",
+  key: "7eea952f1003fc3d6667",
+  secret: "f87b777a7f1ec87a127c",
+  cluster: "mt1",
+  useTLS: true
+});
+
+const app = express()
+
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200' ]  //3000:reactjs  8080:vuejs 4200:angular 
+    
+}))
+
+app.use(express.json())
+
+app.post('/api/messages', async(req,res) => { 
+    await pusher.trigger("chat", "message", {
+        username: req.body.username,
+        message: req.body.message
+    });
+    res.json([])
+})
+
+
+console.log('listening to port 8000');
+app.listen(8000)
+
+
+//www.pusher
+
+
